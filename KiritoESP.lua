abfa832761bb594012c699f2240e5264d8df31f6
samuelLovesPie <86863807+samuelLovesPie@users.noreply.github.com>
2022-04-12 11:29:11 -0400

    Update KiritoESP.lua

--- KiritoESP.lua
+++ KiritoESP.lua
@@ -227,7 +227,7 @@ function boxBase:Update()
         if Vis5 then
             self.Components.Distance.Visible = true
             self.Components.Distance.Position = Vector2.new(TagPos.X, TagPos.Y + 14)
-            self.Components.Distance.Text = math.floor((cam.CFrame.p - cf.p).magnitude) .."m away"
+            self.Components.Distance.Text = math.floor((cam.CFrame.p - cf.p).magnitude) .."m"
             self.Components.Distance.Color = color
 	else
 	    self.Components.Distance.Visible = false
